7b0901812e94e24f502e7cdac6f042ba69231a96
Szymon Jeż <jezyk2796@op.pl>
2018-06-02 00:28:43 +0200

    addBtn works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
+document.addEventListener('DOMContentLoaded', function() {
+	var button = document.querySelector('.addBtn');
+
+	button.addEventListener('click', function() {
+        var item = document.getElementById('task').value;
+        var text = document.createTextNode(item);
+        var newItem = document.createElement('li');
+        newItem.appendChild(text);
+        document.querySelector('ul').appendChild(newItem);
+	});
+});
