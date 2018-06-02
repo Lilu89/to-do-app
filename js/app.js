d7cd43197f2c50ad1d810848ee53dbe893bb1401
Patryk Swiatek <patrykswiatek@Air-Patryk.home>
2018-06-02 18:30:50 +0200

    Home page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+	var add = document.querySelector(".add"),
+		approve = document.querySelector(".approve"),
+		history = document.querySelector(".history"),
+		body = document.querySelector("body");
+
+	var newWindow = document.createElement("div");
+	var newDiv = body.appendChild(newWindow);
+	newDiv.classList.add("hidden");
+	newDiv.setAttribute("id", "tasksHistory");
+
+	add.addEventListener('click', function () {
+
+
+
+	});
+
+	approve.addEventListener('click', function () {
+
+
+	});
+
+	history.addEventListener('click', function () {
+
+		newDiv.classList.remove("hidden");
+		newDiv.classList.add("visible");
+
+
+
+	})
+
+
+});
